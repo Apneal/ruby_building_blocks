b2a9f3ff2f8526f44bb9b5f5b09284d4625a87e7
Angus S. Dobson <omega_x1_@hotmail.com>
2015-07-24 02:22:33 -0400

    substring refactoring

--- substrings.rb
+++ substrings.rb
@@ -1,13 +1,14 @@
-def substrings(input_string, dictionary)
-	string_count = {}
-	dictionary.each do |dictionary_word|
-		input_string.downcase.scan(dictionary_word.downcase) do
-			if string_count.has_key?(dictionary_word)
-				string_count[dictionary_word] += 1
+def substrings(string, dictionary)
+	word_count = {}
+	string = string.downcase
+	dictionary.each do |word|
+		string.scan(word.downcase) do
+			if word_count.has_key?(word)
+				word_count[word] += 1
 			else
-				string_count[dictionary_word] = 1
+				word_count[word] = 1
 			end
 		end	
 	end
-	return string_count
+	return word_count
 end
