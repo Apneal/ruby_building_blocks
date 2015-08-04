167bcfdfd773cdfbd43e659f98962ef0993cffaf
Angus S. Dobson <omega_x1_@hotmail.com>
2015-08-03 21:30:20 -0400

    bubble_sort refactoring

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,26 +1,27 @@
 def bubble_sort(input)
 	loop do
-		i=true
+		sorted=true
 		for j in (0..(input.length-2) )
 			if (input[j] <=> input[j+1]) > 0
-				i = false
+				sorted = false
 				input[j], input[j+1] = input[j+1], input[j]
 			end
 		end
-		break if i
+		break if sorted
 	end
 	return input
 end
+
 def bubble_sort_by(input)
 	loop do
-		i=true
+		sorted=true
 		for j in (0..(input.length-2) )
 			if (yield(input[j],input[j+1])) && (yield(input[j],input[j+1])) > 0
-				i = false
+				sorted = false
 				input[j], input[j+1] = input[j+1], input[j]
 			end
 		end
-		break if i
+		break if sorted
 	end
 	return input
 end
